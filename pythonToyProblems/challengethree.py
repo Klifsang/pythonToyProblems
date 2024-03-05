5a72bfec6faff9fcacb42fe2810ef0abe02c6ca9
Klifsang <152657497+Klifsang@users.noreply.github.com>
2024-03-05 12:12:45 +0300

    Update challengethree.py

--- pythonToyProblems/challengethree.py
+++ pythonToyProblems/challengethree.py
@@ -1,1 +1,14 @@
+import string
 
+alphabet = string.ascii_lowercase
+print(alphabet)
+
+def solution(N):
+    generated_string = []
+    common_occurrence = N//26
+    last_letters = N % 26
+    for i in range(27):
+        generated_string.extend(alphabet[i] * common_occurrence)
+    print(''.join(generated_string))
+
+solution(53)
